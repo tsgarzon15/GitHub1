7100713a73b51c009bb7dc8ab157ab17f49a8a20
tsgarzon15 <42456630+tsgarzon15@users.noreply.github.com>
2018-08-16 21:22:17 -0500

    Archivos cargados via upload

---
+++ Hello GitHub/src/br/com/joseassis/git/HelloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.joseassis.git;
+
+/**
+ * @author sala2
+ *Ejemplo de integracion de Eclipse con GitHub
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Git");
+	}
+
+}
